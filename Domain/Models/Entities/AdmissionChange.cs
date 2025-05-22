6d5c34f36a03959716c5fd2dccbb379703cbe9a9
Skalersaas <efvhdg@gmail.com>
2025-05-22 14:19:05 +0400

    Update AdmissionChange.cs

--- Domain/Models/Entities/AdmissionChange.cs
+++ Domain/Models/Entities/AdmissionChange.cs
@@ -7,7 +7,6 @@ namespace Domain.Models.Entities
         public Guid Id { get; set; }
 
         public Guid AdmissionId { get; set; }
-        public Admission? Admission { get; set; } = null;
         public DateTime CreatedTime { get; set; }
         public JsonDocument Data { get; set; }
     }
